d122bf06ae178902a7fd583103a45abbd7550b9c
James Leu <leujase@gmail.com>
2020-08-31 15:02:50 -0500

    made edits to onPanEnd

--- lib/src/custom_layout.dart
+++ lib/src/custom_layout.dart
@@ -194,19 +194,19 @@ abstract class _CustomLayoutStateBase<T extends _SubSwiper> extends State<T>
     if (_lockScroll) return;
 
     double velocity = widget.scrollDirection == Axis.horizontal
-        ? -details.velocity.pixelsPerSecond.dx //edited here added negative sign
+        ? details.velocity.pixelsPerSecond.dx
         : details.velocity.pixelsPerSecond.dy;
 
     if (_animationController.value >= 0.75 || velocity > 500.0) {
       if (_currentIndex <= 0 && !widget.loop) {
         return;
       }
-      _move(1.0, nextIndex: _currentIndex - 1);
+      _move(1.0, nextIndex: _currentIndex + 1); //edited here, changed from - 1 to + 1
     } else if (_animationController.value < 0.25 || velocity < -500.0) {
       if (_currentIndex >= widget.itemCount - 1 && !widget.loop) {
         return;
       }
-      _move(0.0, nextIndex: _currentIndex + 1);
+      _move(0.0, nextIndex: _currentIndex - 1);  //edited here, changed from - 1 to + 1
     } else {
       _move(0.5);
     }
