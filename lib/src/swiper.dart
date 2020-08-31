e0bbca0293d903892ff8c1fce0d9d9c4e302a1a5
James Leu <leujase@gmail.com>
2020-08-31 14:46:34 -0500

    changed alignment back to centerleft

--- lib/src/swiper.dart
+++ lib/src/swiper.dart
@@ -901,7 +901,7 @@ class _StackViewState extends _CustomLayoutStateBase<_StackSwiper> {
         : new Offset(0.0, f);
 
     Alignment alignment = widget.scrollDirection == Axis.horizontal
-        ? Alignment.centerRight //edited here, changed from left to right
+        ? Alignment.centerLeft //edited here, changed from left to right
         : Alignment.topCenter;
 
     return new Opacity(
